8fe9aeec721be17741f7cd4929dec16c319e4345
Ali.Zandi <ali.zandi2020pc@gmail.com>
2026-06-16 00:24:29 +0330

    Delete Comments

--- Backend/apps/core/urls.py
+++ Backend/apps/core/urls.py
@@ -1,23 +1,4 @@
-# # apps/core/urls.py
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import (
-#     EducationStageViewSet, CourseViewSet, 
-#     YearViewSet, ExamTypeViewSet, CategoryTreeView
-# )
 
-# router = DefaultRouter()
-# router.register('education-stages', EducationStageViewSet, basename='education-stage')
-# router.register('courses', CourseViewSet, basename='course')
-# router.register('years', YearViewSet, basename='year')
-# router.register('exam-types', ExamTypeViewSet, basename='exam-type')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('category-tree/', CategoryTreeView.as_view(), name='category-tree'),
-# ]
-
-# apps/core/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
--- Backend/apps/exam/urls.py
+++ Backend/apps/exam/urls.py
@@ -1,16 +1,3 @@
-# # apps/exam/urls.py
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import ExamViewSet, ExamQuestionViewSet, StudentExamAttemptViewSet
-
-# router = DefaultRouter()
-# router.register('exams', ExamViewSet, basename='exam')
-# router.register('exam-questions', ExamQuestionViewSet, basename='exam-question')
-# router.register('attempts', StudentExamAttemptViewSet, basename='attempt')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
